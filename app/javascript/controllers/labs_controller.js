9465b271ea96e647b772f95e90477321b6cfd2a1
rocLv <wangqsh999@gmail.com>
2021-08-30 15:55:25 +0800

    lab index

--- app/javascript/controllers/labs_controller.js
+++ app/javascript/controllers/labs_controller.js
@@ -1,23 +1,29 @@
 require("xterm/css/xterm.css") 
 import { Terminal } from 'xterm'
 import { AttachAddon   } from 'xterm-addon-attach';
-import { fit } from 'xterm-addon-fit';
+import { FitAddon } from 'xterm-addon-fit';
 import { Toast} from 'bootstrap';
 
 
 
 document.addEventListener("turbolinks:load", () =>{
- // var term = new Terminal();
- // term.open(document.getElementById('terminal'));
- // const socket = new WebSocket('ws://localhost:5020/containers/fe9e7998d947/attach/ws?stream=1&stdout=1');
- // const attachAddon = new AttachAddon(socket);
+ var term = new Terminal();
+ const fitAddon = new FitAddon();
 
- // // Attach the socket to term
- // term.loadAddon(attachAddon);
- // term.writeln("Hello from \x1B[1;3;31mxterm.js\n\x1B[0m");
- // term.write("$ ");
- // 
- // 
+ const socket = new WebSocket('ws://localhost:5020/containers/fe9e7998d947/attach/ws?stream=1&stdout=1');
+ const attachAddon = new AttachAddon(socket);
+
+ term.loadAddon(fitAddon);
+ term.loadAddon(attachAddon);
+
+ term.open(document.getElementById('terminal'));
+ fitAddon.fit()
+
+ // Attach the socket to term
+ term.writeln("Hello from \x1B[1;3;31mxterm.js\n\x1B[0m");
+ term.write("$ ");
+ 
+ 
 
  // var toastTrigger = document.getElementById('liveToastBtn')
  // var toastLiveExample = document.getElementById('liveToast')
